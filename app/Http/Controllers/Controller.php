4fbb7b4ba4bc76be8a9dc493e9657cece57432ae
geobas <ksenera@yahoo.com>
2021-11-30 10:29:18 +0000

    define some controllers

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,26 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Throwable;
+use Log;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Log error.
+     *
+     * @param  Throwable  $t
+     * @return void
+     *
+     * @throws \Exception|\Error
+     */
+    protected function logError(Throwable $t): void
+    {
+        Log::error("'" . $t->getMessage() . "' - File: '" . $t->getFile() . "' - Method: '" . 
+            debug_backtrace()[1]['function'] . "' at line: " . $t->getLine());
+
+        throw $t;
+    }
 }
